0269ad2751600a88e5c3f181edb44bec9c1569e1
Viraat Das <viraat.laldas@gmail.com>
2019-05-06 19:16:01 -0400

    properly formatted two dataframe and wrote in a csv

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ test_df = pd.read_csv("test.csv")
 
 #Preprocess
 cols = test_df.columns.tolist()
-cols = cols[0:7] + cols[8:9] + cols[10:]
+cols = cols[0:2] + cols[3:7] + cols[8:9] + cols[10:]
 test_df = test_df[cols]
 
 #clean the data
@@ -16,8 +16,8 @@ test_df = test_df.dropna()
 #get pass_id
 cols = test_df.columns.tolist()
 p_cols = cols[0:1]
-pass_id = test_df[p_cols]
-cols = cols[1:2] + cols[3:]
+pass_df = test_df[p_cols]
+cols = cols[1:]
 test_df = test_df[cols]
 
 test_df = preprocess(test_df)
@@ -25,5 +25,9 @@ test_df = preprocess(test_df)
 predict = clf.predict(test_df)
 
 predict_df = pd.DataFrame({"Survived": predict})
-print(pass_id)
-print(predict_df)
+
+pass_df.index = range(331)
+out_df = pd.concat([pass_df, predict_df], axis=1)
+out_df.to_csv("out.csv")
+
+
