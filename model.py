e9b2ca674b9615ee52af3a5bd7d9bbc79dde27d7
Viraat Das <viraat.laldas@gmail.com>
2019-05-06 02:46:52 -0400

    Properly cleaned data and added proper classification to categorical data

--- model.py
+++ model.py
@@ -1,13 +1,44 @@
 import pandas as pd
 from sklearn import svm
 from sklearn.model_selection import train_test_split
+import numpy as np
 
 #Load Data
 train_df = pd.read_csv("train.csv")
 
 #Reordering Data and getting the useful ones
 cols = train_df.columns.tolist()
-cols = cols[2:3] + cols[4:] + cols[1:2]
+cols = cols[2:3] + cols[4:8] + cols[9:10] + cols[11:12] + cols[1:2]
 train_df = train_df[cols]
 
-# X_train, X_test, y_train, y_Test = train_test_split(train_df[2:], train_df[1], test_size=0.2)
+age_avg = train_df['Age'].mean()
+
+for index, row in train_df.iterrows():
+
+    #Make all Nan age the average
+    if np.isnan(row[2]):
+        train_df.at[index, 'Age'] = age_avg
+
+    #Replace male with 0 and female with 1
+    if row[1] == 'male':
+        train_df.at[index, 'Sex'] = 0
+    else:
+        train_df.at[index, 'Sex'] = 1
+
+    #Replacing embarked destinations with numeric values
+    if row[6] == 'C':
+        train_df.at[index, 'Embarked'] = 0
+    elif row[6] == 'Q':
+        train_df.at[index, 'Embarked'] = 1
+    elif row[6] == 'S':
+        train_df.at[index, 'Embarked'] = 2
+
+print(train_df)
+
+
+#Split into train and test
+X_train, X_test, y_train, y_Test = train_test_split(train_df.iloc[:, 0:-1], train_df.iloc[:,-1], test_size=0.2)
+
+#SVC gamma scale
+# clf = svm.SVC(gamma='scale')
+# clf.fit(X_train, y_train)
